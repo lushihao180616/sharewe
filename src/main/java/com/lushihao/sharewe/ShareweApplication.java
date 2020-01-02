aca6df9875cdea15dc63471086d14590fc6e52ea
lushihao <lushihaomb@qq.com>
2020-01-02 20:45:05 +0800

    chushihua

---
+++ src/main/java/com/lushihao/sharewe/ShareweApplication.java
@@ -0,0 +1,13 @@
+package com.lushihao.sharewe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareweApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lushihao/sharewe/ShareweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lushihao.sharewe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
