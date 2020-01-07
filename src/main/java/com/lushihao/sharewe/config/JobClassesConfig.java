a1a6e4572c1a3e715266e6a5e8757d5edae35de4
lushihao <12345678>
2020-01-07 17:18:58 +0800

    修改注释

--- src/main/java/com/lushihao/sharewe/config/JobClassesConfig.java
+++ src/main/java/com/lushihao/sharewe/config/JobClassesConfig.java
@@ -11,21 +11,8 @@ import java.util.Map;
 @Component      //不加这个注解的话, 使用@Autowired 就不能注入进去了
 @ConfigurationProperties(prefix = "jobclass")  // 配置文件中的前缀
 public class JobClassesConfig {
-    /**
-     * 从配置文件中读取的jobMap开头的数据
-     * 注意：名称必须与配置文件中保持一致
-     */
-    private Map<String, String> jobMap = new HashMap<>();
 
     private List<Map<String, String>> jobList = new ArrayList<>();
-
-    public Map<String, String> getJobMap() {
-        return jobMap;
-    }
-
-    public void setJobMap(Map<String, String> jobMap) {
-        this.jobMap = jobMap;
-    }
 
     public List<Map<String, String>> getJobList() {
         return jobList;
