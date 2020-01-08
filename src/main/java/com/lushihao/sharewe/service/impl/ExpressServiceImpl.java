0ae67b32253b7e653c546b125e05064d8d0181d8
lushihao <12345678>
2020-01-08 11:18:08 +0800

    修改注释

--- src/main/java/com/lushihao/sharewe/service/impl/ExpressServiceImpl.java
+++ src/main/java/com/lushihao/sharewe/service/impl/ExpressServiceImpl.java
@@ -1,11 +1,11 @@
 package com.lushihao.sharewe.service.impl;
 
 import com.lushihao.sharewe.dao.ExpressMapper;
-import com.lushihao.sharewe.dao.PurchaseMapper;
 import com.lushihao.sharewe.entity.express.Express;
 import com.lushihao.sharewe.service.ExpressService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -16,7 +16,14 @@ public class ExpressServiceImpl implements ExpressService {
     @Resource
     private ExpressMapper expressMapper;
 
+    /**
+     * 发送快递
+     *
+     * @param express
+     * @return
+     */
     @Override
+    @Transactional
     public String sendExpress(Express express) {
         return null;
     }
