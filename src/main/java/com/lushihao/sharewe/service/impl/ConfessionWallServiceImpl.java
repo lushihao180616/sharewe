0115a3d103af92a43c4f8589ba9e295f28639d49
lushihao <12345678>
2020-01-15 17:27:37 +0800

    添加告白墙信息

--- src/main/java/com/lushihao/sharewe/service/impl/ConfessionWallServiceImpl.java
+++ src/main/java/com/lushihao/sharewe/service/impl/ConfessionWallServiceImpl.java
@@ -4,12 +4,20 @@ import com.lushihao.sharewe.entity.confession.ConfessionWall;
 import com.lushihao.sharewe.service.ConfessionWallService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @EnableTransactionManagement
 public class ConfessionWallServiceImpl implements ConfessionWallService {
 
+    /**
+     * 发送告白墙
+     *
+     * @param confessionWall
+     * @return
+     */
     @Override
+    @Transactional
     public String sendWall(ConfessionWall confessionWall) {
         return null;
     }
