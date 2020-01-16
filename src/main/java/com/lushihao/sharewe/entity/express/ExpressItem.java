5cfaf66ae42fb3105a21f1a5733306c1f78df1fb
lushihao <12345678>
2020-01-16 09:17:15 +0800

    添加数据

--- src/main/java/com/lushihao/sharewe/entity/express/ExpressItem.java
+++ src/main/java/com/lushihao/sharewe/entity/express/ExpressItem.java
@@ -2,65 +2,77 @@ package com.lushihao.sharewe.entity.express;
 
 public class ExpressItem {
 
-	/**
-	 * 标识
-	 */
-	private int id;
-	/**
-	 * 验证码
-	 */
-	private String code;
-	/**
-	 * 昵称
-	 */
-	private String name;
-	/**
-	 * 手机尾号
-	 */
-	private String number;
-	/**
-	 * 快递
-	 */
-	private int expressId;
+    /**
+     * 标识
+     */
+    private int id;
+    /**
+     * 验证码
+     */
+    private String code;
+    /**
+     * 昵称
+     */
+    private String name;
+    /**
+     * 手机尾号
+     */
+    private String number;
+    /**
+     * 快递类型
+     */
+    private ExpressType expressType;
+    /**
+     * 快递
+     */
+    private int expressId;
 
-	public int getId() {
-		return id;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+	public ExpressType getExpressType() {
+		return expressType;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getNumber() {
-		return number;
-	}
-
-	public void setNumber(String number) {
-		this.number = number;
+	public void setExpressType(ExpressType expressType) {
+		this.expressType = expressType;
 	}
 
 	public int getExpressId() {
-		return expressId;
-	}
+        return expressId;
+    }
 
-	public void setExpressId(int expressId) {
-		this.expressId = expressId;
-	}
+    public void setExpressId(int expressId) {
+        this.expressId = expressId;
+    }
 
 }
